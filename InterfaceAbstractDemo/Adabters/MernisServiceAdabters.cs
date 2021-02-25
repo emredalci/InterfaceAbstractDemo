a7bae2658d04dd85c438114d45256ac8e397f9f8
Emre <emredalc@gmail.com>
2021-02-25 22:06:26 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Adabters/MernisServiceAdabters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+
+namespace InterfaceAbstractDemo.Adabters
+{
+    class MernisServiceAdabters : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync
+
+         (new TCKimlikNoDogrulaRequest
+         (new TCKimlikNoDogrulaRequestBody(Convert.ToInt64(customer.NationalityId), customer.FirstName, customer.LastName, customer.DateOfBirth.Year))).Result.Body.TCKimlikNoDogrulaResult;
+
+
+
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using InterfaceAbstractDemo.Adabters;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdabters());
+            customerManager.Save(new Customer {DateOfBirth = new DateTime(1995,4,13), FirstName="Emre",LastName="Dalcı",NationalityId="123456798" });
+            Console.ReadLine();
+        }
+    }
+}
